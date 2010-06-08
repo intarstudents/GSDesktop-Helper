0cf1a6ceabc558dc35e135779b0d592828cae494
Intars <the.mobix@gmail.com>
2010-06-08 17:17:08 +0300

    Added StatusIcons menu and about dialog

--- GSDesktop-Helper.py
+++ GSDesktop-Helper.py
@@ -44,6 +44,12 @@ class GSDesktop_Helper:
       except Exception, e:
         print e
     
+    self._status_icon = gtk.StatusIcon()
+    self._status_icon.set_from_file("gs128.png")
+    self._status_icon.set_tooltip("GSDesktop Helper")
+    #self._status_icon.connect("activate", self.menu_callback)
+    self._status_icon.connect("popup-menu", self.menu_callback)
+
     self._window = gtk.Window()
     self._window.connect('destroy', gtk.main_quit)
     
@@ -52,9 +58,40 @@ class GSDesktop_Helper:
     
   def keyboard_callback(self, toggle):
     if os.path.exists(self._shortcutAction) and os.access(self._shortcutAction, os.W_OK):
-      file = open(self._shortcutAction, "a")
-      file.write("%s\n" % toggle)
-      file.close()
+      try:
+        file = open(self._shortcutAction, "a")
+        file.write("%s\n" % toggle)
+        file.close()
+      except Exception, e:
+        print e
+  
+  def menu_callback(self, icon, button, time):
+    menu = gtk.Menu()
+    
+    restore = gtk.MenuItem("Restore")
+    about   = gtk.MenuItem("About")
+    quit    = gtk.MenuItem("Quit")
+    
+    about.connect("activate", self.show_about_dialog)
+    quit.connect("activate", gtk.main_quit)
+    
+    menu.append(restore)
+    menu.append(about)
+    menu.append(quit)
+    
+    menu.show_all()
+    menu.popup(None, None, gtk.status_icon_position_menu, button, time, self._status_icon)
+    
+  def show_about_dialog(self, widget):
+    about_dialog = gtk.AboutDialog()
+    
+    about_dialog.set_destroy_with_parent(True)
+    about_dialog.set_name("GSDesktop Helper")
+    about_dialog.set_version("1.0")
+    about_dialog.set_authors(["Intars Students"])
+    
+    about_dialog.run()
+    about_dialog.destroy()
 
 if __name__ == '__main__':
   GSDesktop_Helper()
