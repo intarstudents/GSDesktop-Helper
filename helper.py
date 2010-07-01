c2f7d6f7690be7bb7fcc8d6205a605708cf1fe57
Intars <the.mobix@gmail.com>
2010-07-01 15:50:12 +0300

    Bug fixes and ready to ship 0.2

--- helper.py
+++ helper.py
@@ -6,13 +6,15 @@ import sys
 import re
 import gtk
 import keybinder
+import ConfigParser
 
 class GSDesktop_Helper:
   def __init__(self):
     
     self._NAME    = "GSDesktop Helper"
-    self._VERSION = "0.1"
+    self._VERSION = "0.2"
     self._AUTHORS = ["Intars Students"]
+    self._INI     = os.path.expanduser('~')+"/.gsdesktop-helper"
     
     # Try to find icon file
     if os.path.exists("%s/gsd-helper.png" % sys.path[0]):
@@ -53,7 +55,7 @@ class GSDesktop_Helper:
       os._exit(0)
     
     # Default list of all hotkeys and their actions
-    self._hotkeys = {
+    self._defaults = {
       "next"            : "<Ctrl>period",         # Ctrl + .
       "previous"        : "<Ctrl>comma",          # Ctrl + ,
       "playpause"       : "<Ctrl>equal",          # Ctrl + =
@@ -64,6 +66,12 @@ class GSDesktop_Helper:
       "volumedown"      : "<Ctrl><Alt>Page_Down", # Ctrl + Alt + Page Down
     }
     
+    # Load default from default hotkeys
+    self._hotkeys = {}
+    for toggle in self._defaults:
+      self._hotkeys[toggle] = self._defaults[toggle]
+    
+    # Nice names for keyboard shortcuts
     self._hotkey_name = {
       "next"            : "Plays next song",
       "previous"        : "Plays previous song",
@@ -75,8 +83,8 @@ class GSDesktop_Helper:
       "volumedown"      : "Decreases volume",
     }
     
+    self.load_conf()
     self.bindKeys()
-    self.modify_toggle = None
     self.create_conf_window()
     
     # Status icon stuff
@@ -87,10 +95,32 @@ class GSDesktop_Helper:
     
     gtk.main()
   
+  # If configuration file exists, load key combos from it 
+  def load_conf(self):
+    try:
+      config = ConfigParser.ConfigParser()
+      config.read(self._INI)
+      
+      for toggle in config.options("hotkeys"):
+        if toggle in self._hotkeys.keys():
+          self._hotkeys[toggle] = config.get("hotkeys", toggle)
+          
+    except: pass
+  
+  # If possible, save to configuration file
   def save_conf(self):
-    pass
+    configFile = open(self._INI,"w")
+    
+    config = ConfigParser.ConfigParser()
+    config.add_section("hotkeys")
+    
+    for toggle in self._hotkeys:
+      config.set("hotkeys", toggle, self._hotkeys[toggle])
+      
+    config.write(configFile)
   
   def create_conf_window(self):
+    # Look and feel of window
     self._window = gtk.Window(gtk.WINDOW_TOPLEVEL)
     self._window.set_title(self._NAME)
     self._window.set_default_size(320, 350)
@@ -98,20 +128,27 @@ class GSDesktop_Helper:
     self._window.set_position(gtk.WIN_POS_CENTER_ALWAYS)
     self._window.set_border_width(10)
     
+    # Hooks for it (on close and key press)
     self._window.connect("destroy", self.hide_conf_window)
     self._window.connect('key-press-event', self.change_toggle)
     
-    self._confRowBox  = gtk.VBox(False, 0)
-    
+    # Build warning label
     warningLabel = gtk.Label()
-    warningLabel.set_markup('<span size="x-small" foreground="#FFF" background="#AE0000">Keyboard shortcuts are disabled while this window is open.</span>');
+    warningLabel.set_markup('<span size="x-small" foreground="#FFF" background="#AE0000">Keyboard shortcuts are disabled, while this window is open.</span>');
     warningLabel.show()
     
+    # Currently active input box
+    self.modify_toggle = None
+    
+    # Row handler
+    self._confRowBox  = gtk.VBox(False, 0)
     self._confRowBox.pack_end(warningLabel)
     
+    # Allow to edit each toggle
     for toggle in self._hotkeys:
       box = gtk.HBox(False, 0)
       
+      # Kinda hacky way, err?
       seperator = gtk.Label("")
       seperator.show()
       
@@ -122,12 +159,14 @@ class GSDesktop_Helper:
       keyval, state = gtk.accelerator_parse(self._hotkeys[toggle])
       keycombo = gtk.accelerator_get_label(keyval, state)
       
+      # Input box with click event to change active toggle
       entry = gtk.Entry()
       entry.set_editable(False)
       entry.set_text(keycombo)
       entry.connect("event", self.focus_toggle, toggle, entry)
       entry.show()
       
+      # Pack it up
       box.pack_start(label, False, False, 0)
       box.pack_start(seperator)
       box.pack_start(entry, False, False, 0)
@@ -135,11 +174,13 @@ class GSDesktop_Helper:
       
       self._confRowBox.pack_start(box, True, True, 0)
     
+    # Ok everything is ready, append!
     self._confRowBox.show()
     self._window.add(self._confRowBox)
   
+  # Change toggle (if it isn't same as previous)
   def change_toggle(self, window, event):
-    if not self.modify_toggle == None:
+    if self.modify_toggle != None:
       entry, toggle = self.modify_toggle
       
       keyval = event.keyval
@@ -147,26 +188,36 @@ class GSDesktop_Helper:
       keycombo = gtk.accelerator_get_label(keyval, state)
       new_toggle = gtk.accelerator_name(keyval, state)
       
-      self._hotkeys[toggle] = new_toggle
-      entry.set_text(keycombo)
+      if not self._hotkeys[toggle] == new_toggle: 
+        self._hotkeys[toggle] = new_toggle
+        entry.set_text(keycombo)
+        self.save_conf()
   
+  # Give focus to toggle you want to change
   def focus_toggle(self, widget, event, toggle, entry, data=None):
     if event.type == gtk.gdk.BUTTON_RELEASE:
       self.modify_toggle = (entry, toggle)
   
+  # On window show unbind keys
   def show_conf_window(self, window):
     self.unbindKeys()
     self._window.show()
   
+  # On window hide bind keys
   def hide_conf_window(self, window):
     self.bindKeys()
     self._window.hide()
   
+  # Handle binding of keys
   def bindKeys(self):
     for toggle in self._hotkeys:  
       try:  keybinder.unbind(self._hotkeys[toggle])
       except: pass
-        
+      
+      # Default bad hotkeys
+      if not gtk.accelerator_parse(self._hotkeys[toggle])[0] and not self._hotkeys[toggle] == self._defaults[toggle]:
+        self._hotkeys[toggle] = self._defaults[toggle]
+      
       try:
         keybinder.bind(self._hotkeys[toggle], self.keyboard_callback, toggle)
         print "%s binded" % self._hotkeys[toggle]
@@ -174,6 +225,7 @@ class GSDesktop_Helper:
       except Exception, e:
         pass
   
+  # Handle unbinding of keys
   def unbindKeys(self):
     for toggle in self._hotkeys:  
       try:  keybinder.unbind(self._hotkeys[toggle])
